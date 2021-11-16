039996a3f6f765e331d5a8ac2035e82832ad9218
romnnn <roman.dahm@gmx.de>
2021-11-16 22:45:55 +0100

    migrate to debian based manylinux for musllinux compat

--- python/compile-protoc.sh
+++ python/compile-protoc.sh
@@ -2,17 +2,20 @@
 
 set -e
 
-# gcc-c++ is g++ under apt
-yum install -y autoconf \
-  automake \
-  libtool \
-  gzip \
-  make \
-  wget \
-  gcc-c++ \
-  glibc-static \
-  libstdc++-static \
-  git
+# yum install -y autoconf \
+#   automake \
+#   libtool \
+#   gzip \
+#   make \
+#   wget \
+#   gcc-c++ \
+#   glibc-static \
+#   libstdc++-static \
+#   git
+
+apt-get update
+apt-get install -y autoconf \
+  automake libtool gzip make wget g++ git
 
 git clone https://github.com/protocolbuffers/protobuf.git protobuf
 cd protobuf
@@ -39,17 +42,7 @@ fi
 echo $CXXFLAGS
 echo $LDFLAGS
 
-# make distclean
 ./configure --prefix=/usr --disable-shared
-make -B -j8 || echo "" > /dev/null
+make -j8
 make install
 which protoc
-
-# export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib"
-# ./build-protoc.sh linux ppcle_64 protoc
-# ./build-protoc.sh linux s390_64 protoc
-# file target/linux/ppcle_64/protoc.exe
-# file target/linux/s390_64/protoc.exe
-# cp target/linux/ppcle_64/protoc.exe $GITHUB_WORKSPACE/ppcle_64_protoc
-# cp target/linux/s390_64/protoc.exe $GITHUB_WORKSPACE/s390_64_protoc
-# echo $(realpath target/linux/ppcle_64/protoc.exe)

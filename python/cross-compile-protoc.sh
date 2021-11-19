a8b79884677d7165388ed7b65b65ab409b88ea93
romnnn <roman.dahm@gmx.de>
2021-11-19 01:58:18 +0100

    add musllinux

--- python/cross-compile-protoc.sh
+++ python/cross-compile-protoc.sh
@@ -9,24 +9,28 @@ OUT="${DIR}/protoc"
 PROTOBUF_VERSION="v3.19.1"
 
 mkdir -p ${OUT}
-for ARCH in x86_64 i686 aarch64 ppc64le s390x
+for CONTAINER_TYPE in musllinux_1_1 manylinux2014 
 do
-  CONTAINER="quay.io/pypa/manylinux2014_${ARCH}"
-  echo "building ${CONTAINER} ..."
+  for ARCH in x86_64 i686 aarch64 ppc64le s390x
+  do
+    CONTAINER="quay.io/pypa/${CONTAINER_TYPE}_${ARCH}"
+    echo "building ${CONTAINER} ..."
 
-  rm -rf ${BUILD}
-  mkdir -p ${BUILD}
+    rm -rf ${BUILD}
+    mkdir -p ${BUILD}
 
-  docker run \
-    -v ${BUILD}:/out \
-    -v ${DIR}/compile-protoc.sh:/compile.sh \
-    -v ${DIR}/install-dependencies.sh:/install.sh \
-    -e PROTOBUF_VERSION="${PROTOBUF_VERSION}" \
-    -e PREFIX="/out" \
-    ${CONTAINER} \
-    /bin/bash -c "bash install.sh && bash compile.sh"
+    docker run \
+      -v ${BUILD}:/out \
+      -v ${DIR}/compile-protoc.sh:/compile.sh \
+      -v ${DIR}/install-dependencies.sh:/install.sh \
+      -e PROTOBUF_VERSION="${PROTOBUF_VERSION}" \
+      -e PREFIX="/out" \
+      ${CONTAINER} \
+      /bin/bash -c "bash install.sh && bash compile.sh"
 
-  cp "${BUILD}/bin/protoc" "$OUT/${ARCH}_protoc"
-  rm -rf ${BUILD}
+    cp "${BUILD}/bin/protoc" "$OUT/${CONTAINER_TYPE}_${ARCH}_protoc"
+    zip "$OUT/${CONTAINER_TYPE}_${ARCH}_protoc.zip" "${BUILD}/bin/protoc"
+    rm -rf ${BUILD}
 
+  done
 done
